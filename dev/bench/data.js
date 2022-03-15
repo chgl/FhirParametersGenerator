79c22740f1a17528e158b8227a915ea0bb275cb0
github-action-benchmark <github@users.noreply.github.com>
2022-03-15 00:23:20 +0000

    add Benchmark.Net Benchmark (benchmarkdotnet) benchmark result for d301aa9cfa1eaaf3c6ed09e14690858e2bd128ff

---
+++ dev/bench/data.js
@@ -0,0 +1,38 @@
+window.BENCHMARK_DATA = {
+  "lastUpdate": 1647303800275,
+  "repoUrl": "https://github.com/chgl/FhirParametersGenerator",
+  "entries": {
+    "Benchmark.Net Benchmark": [
+      {
+        "commit": {
+          "author": {
+            "email": "chgl@users.noreply.github.com",
+            "name": "chgl",
+            "username": "chgl"
+          },
+          "committer": {
+            "email": "noreply@github.com",
+            "name": "GitHub",
+            "username": "web-flow"
+          },
+          "distinct": true,
+          "id": "d301aa9cfa1eaaf3c6ed09e14690858e2bd128ff",
+          "message": "chore: added basic benchmark project (#15)",
+          "timestamp": "2022-03-15T01:21:53+01:00",
+          "tree_id": "f4fd8f191b29145e58905a8c539194fd71d5b781",
+          "url": "https://github.com/chgl/FhirParametersGenerator/commit/d301aa9cfa1eaaf3c6ed09e14690858e2bd128ff"
+        },
+        "date": 1647303799709,
+        "tool": "benchmarkdotnet",
+        "benches": [
+          {
+            "name": "Benchmarks.GenerateSourceCode",
+            "value": 4147126.9989583334,
+            "unit": "ns",
+            "range": "± 51467.82846829187"
+          }
+        ]
+      }
+    ]
+  }
+}
