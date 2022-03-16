8e8808f73e3cebe4323380e5cdfafae46bd7f165
github-action-benchmark <github@users.noreply.github.com>
2022-03-16 22:30:29 +0000

    add Benchmark.Net Benchmark (benchmarkdotnet) benchmark result for 8cd1dd12efcf8b85ace2bbefe826b9d9f4c99f65

--- dev/bench/data.js
+++ dev/bench/data.js
@@ -1,5 +1,5 @@
 window.BENCHMARK_DATA = {
-  "lastUpdate": 1647303800275,
+  "lastUpdate": 1647469829553,
   "repoUrl": "https://github.com/chgl/FhirParametersGenerator",
   "entries": {
     "Benchmark.Net Benchmark": [
@@ -30,6 +30,36 @@ window.BENCHMARK_DATA = {
             "value": 4147126.9989583334,
             "unit": "ns",
             "range": "± 51467.82846829187"
+          }
+        ]
+      },
+      {
+        "commit": {
+          "author": {
+            "email": "29139614+renovate[bot]@users.noreply.github.com",
+            "name": "renovate[bot]",
+            "username": "renovate[bot]"
+          },
+          "committer": {
+            "email": "noreply@github.com",
+            "name": "GitHub",
+            "username": "web-flow"
+          },
+          "distinct": true,
+          "id": "8cd1dd12efcf8b85ace2bbefe826b9d9f4c99f65",
+          "message": "chore(deps): update dependency verify.xunit to v16.4.2 (#16)\n\n* chore(deps): update dependency verify.xunit to v16.4.2\r\n\r\n* Update ci.yaml\r\n\r\nCo-authored-by: Renovate Bot <bot@renovateapp.com>\r\nCo-authored-by: chgl <chgl@users.noreply.github.com>",
+          "timestamp": "2022-03-16T23:28:56+01:00",
+          "tree_id": "ae44aaded632267fb6511072373ae3fb5d62f0b4",
+          "url": "https://github.com/chgl/FhirParametersGenerator/commit/8cd1dd12efcf8b85ace2bbefe826b9d9f4c99f65"
+        },
+        "date": 1647469828784,
+        "tool": "benchmarkdotnet",
+        "benches": [
+          {
+            "name": "Benchmarks.GenerateSourceCode",
+            "value": 4526902.825,
+            "unit": "ns",
+            "range": "± 413411.0972344621"
           }
         ]
       }
